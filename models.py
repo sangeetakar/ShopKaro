3a7bf54e2b4011cea0322b6c537c771412bef0fe
sangeetakar <1ms21ec095@msrit.edu>
2024-07-29 02:01:12 +0530

    Created Profile Page ,Logout,authotization and decorators

--- models.py
+++ models.py
@@ -1,54 +1,63 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
-db=SQLAlchemy(app)
-#db inherits config.py as it was run before in app.py
+db = SQLAlchemy(app)
+# db inherits config.py as it was run before in app.py
 
 class User(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(32),unique=True)
-    passhash=db.Column(db.String(256),nullable=False)
-    name=db.Column(db.String(64),nullable=True)
-    is_admin=db.Column(db.Boolean,nullable=False,default=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    passhash = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(64), nullable=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
 class Category(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(32),unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True)
 
-    product=db.relationship('Product',backref='category',lazy=True)   #Realationship name: product Whenever we do category .product it will automatically fetch all products in that categroy
-#category to products :One to many relationship
+    product = db.relationship('Product', backref='category', lazy=True)  # Relationship name: product
+    # category to products: One to many relationship
 
 class Product(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(64),nullable=True)
-    price=db.Column(db.Float,nullable=False)
-    category_id=db.Column(db.Integer,db.ForeignKey('category.id'),nullable=False)
-    quantity=db.Column(db.Integer,nullable=False)
-    man_date=db.Column(db.Date,nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    man_date = db.Column(db.Date, nullable=False)
 
-    carts=db.relationship('Cart',backref='product',lazy=True)
-    orders=db.relationship('Order',backref='product',lazy=True)
+    carts = db.relationship('Cart', backref='product', lazy=True)
+    orders = db.relationship('Order', backref='product', lazy=True)
 
 class Cart(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
-    product_id=db.Column(db.Integer,db.ForeignKey('product.id'),nullable=False)
-    quantity=db.Column(db.Integer,nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
 class Transaction(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
-    datetime=db.Column(db.DateTime,nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False)
 
-    orders=db.relationship('Order',backref='transaction',lazy=True)
-
+    orders = db.relationship('Order', backref='transaction', lazy=True)
 
 class Order(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    transaction_id=db.Column(db.Integer,db.ForeignKey('transaction.id'),nullable=False)
-    product_id=db.Column(db.Integer,db.ForeignKey('product.id'),nullable=False)
-    quantity=db.Column(db.Integer,nullable=False)
-    price=db.Column(db.Float,nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
-with app.app_context():
+with app.app_context(): #WAITING for flask app to be ready
     db.create_all()
+
+   # if admin exists ,else create admin
+    admin=User.query.filter_by(is_admin=True).first()
+    if not admin:
+        password_hash=generate_password_hash('admin')
+        admin=User(username='admin',passhash=password_hash, name="Admin",is_admin=True)
+        db.session.add(admin)
+        db.session.commit()
+        
--- routes.py
+++ routes.py
@@ -1,11 +1,11 @@
-from flask import render_template,request,redirect,url_for,flash
+from flask import render_template,request,redirect,url_for,flash,session
 from app import app
 from models import User,db
 from werkzeug.security import generate_password_hash,check_password_hash
+from functools import wraps
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+         
+
 
 @app.route('/login')
 def login():
@@ -30,6 +30,8 @@ def login_post():
         flash('Password is incorrect')
         return redirect(url_for('login'))
     
+    session['user_id']=user.id
+    flash('Login successful')   
     return redirect(url_for('index'))
 
 @app.route('/register')
@@ -62,3 +64,89 @@ def register_post():
     db.session.commit()
 
     return redirect(url_for('login'))
+
+
+'''Creating authorization function which redirects to login page'''
+
+def auth_required(func):
+    @wraps(func) #so that auth function has different name for the / and profile respectivly
+    def inner(*args,**kwargs):
+        if 'user_id' in session:
+            return func(*args,**kwargs)
+        else:
+            flash('Login to continue')
+            return redirect(url_for('login'))
+    return inner
+
+@app.route('/')
+@auth_required
+def index():
+    #user_id in session ;TO CHECK COOKIE
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+@auth_required
+def profile():
+    user=User.query.get(session['user_id'])
+    return render_template('profile.html',user=user)
+
+
+'''@app.route('/')
+def index():
+    #user_id in session ;TO CHECK COOKIE
+    if 'user_id' in session:
+        return render_template('index.html')
+    else:
+        flash('Login to continue')
+        return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():
+    if 'user_id' in session:
+        user=User.query.get(session['user_id'])
+        return render_template('profile.html',user=user)
+    else:
+        flash('Login to continue')
+        return redirect(url_for('login'))'''
+
+
+@app.route('/profile',methods=['POST'])
+@auth_required
+def profile_post():
+    username=request.form.get('username')
+    cpassword=request.form.get('cpassword')
+    password=request.form.get('password')
+    name=request.form.get('name')
+
+    if not username or not cpassword or not password:
+        flash('Please fill the necessary fields')
+        return redirect(url_for('profile'))
+    
+    user=User.query.get(session['user_id'])
+    if not check_password_hash(user.passhash,cpassword):
+        flash('Current password is incorrect')
+        return redirect(url_for('profile'))
+    
+    if username!=user.username:
+        new_username=User.query.filter_by(username=username).first()
+        if new_username:
+            flash('Username already exists')
+            return redirect(url_for('profile'))
+    
+    new_password_hash=generate_password_hash(password)
+    user.username=username
+    user.passhash=new_password_hash
+    user.name=name
+
+    db.session.commit()
+    flash('Profile updated successfully')
+    return redirect(url_for('profile'))
+
+
+@app.route('/logout')
+@auth_required
+def logout():
+    session.pop('user_id')
+    flash('Logged out successfully')
+    return redirect(url_for('login'))
+
